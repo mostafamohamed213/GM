3dd06439c6742d26b68c57582b536734cc92fec7
MahmoudNasrAbdelkader <56201323+MahmoudNasrAbdelkader@users.noreply.github.com>
2022-01-09 16:10:44 +0200

    Purchasing

---
+++ Cars/Models/StatusLogDocument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class StatusLogDocument
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long StatusLogDocumentID { get; set; }
+        [Required]
+        public string Path { get; set; }
+        public string Details { get; set; }
+        public long OrderDetailsStatusLogID { get; set; }
+        [ForeignKey("OrderDetailsStatusLogID")]
+        public OrderDetailsStatusLog OrderDetailsStatusLog { get; set; }
+        [Required]
+        public string SystemUserID { get; set; }
+        public DateTime DTsCreate { get; set; }
+    }
+}

d46a7f23b3899dff6030cd87d3c4842ee803a0e8
Soltan Salah <ssas2584@gmail.com>
2022-02-16 13:09:48 +0200

    add_teamduration_and_teammemerallowed_models

---
+++ Cars/Models/TeamDuration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class TeamDuration
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int TeamDurationID { get; set; }
+        public string Roleid { get; set; }
+
+        public double Duration { get; set; }
+        public Boolean isAssigned { get; set; }
+
+        public string Userid { get; set; }
+
+
+        [ForeignKey("Roleid")]
+        public virtual IdentityRole IdentityRole { get; set; }
+    }
+}
